45121e8729d39ad50fb5a0924fb63875f39083d2
Tate McCormick <tate@kinematic.games>
2021-04-02 14:16:27 -0400

    Add NetworkWorld class

---
+++ LazyECS.Networking/Runtime/NetworkWorld.cs
@@ -0,0 +1,25 @@
+﻿using LazyECS;
+using LazyECS.Component;
+using LazyECS.Entity;
+using UnityEngine;
+
+public class NetworkWorld : World
+{
+	public override void ComponentAddedToEntity(IEntity entity, IComponent component)
+	{
+		base.ComponentAddedToEntity(entity, component);
+		Debug.Log("A component was added");
+	}
+
+	public override void ComponentRemovedFromEntity(IEntity entity, IComponent component)
+	{
+		base.ComponentRemovedFromEntity(entity, component);
+		Debug.Log("A component was removed");
+	}
+
+	public override void ComponentSetOnEntity(IEntity entity, IComponent component)
+	{
+		base.ComponentSetOnEntity(entity, component);
+		Debug.Log("A component was set");
+	}
+}
