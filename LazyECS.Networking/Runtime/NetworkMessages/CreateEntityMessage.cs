63bb9423a170f95111b62c69c400d5cb55d7107c
Tate McCormick <tate@kinematic.games>
2021-04-02 20:26:18 -0400

    Get entity creation syncing

---
+++ LazyECS.Networking/Runtime/NetworkMessages/CreateEntityMessage.cs
@@ -0,0 +1,9 @@
+﻿using Mirror;
+
+namespace NetworkMessages
+{
+	public struct CreateEntityMessage : NetworkMessage
+	{
+		public int id;
+	}
+}
--- LazyECS.Networking/Runtime/NetworkWorld.cs
+++ LazyECS.Networking/Runtime/NetworkWorld.cs
@@ -1,25 +1,58 @@
 ﻿using LazyECS;
 using LazyECS.Component;
 using LazyECS.Entity;
+using Mirror;
+using NetworkMessages;
 using UnityEngine;
 
 public class NetworkWorld : World
 {
-	public override void ComponentAddedToEntity(IEntity entity, IComponent component)
+	public override void Init()
 	{
-		base.ComponentAddedToEntity(entity, component);
+		base.Init();
+		NetworkClient.RegisterHandler<CreateEntityMessage>(CreateEntityMessageReceived);
+	}
+
+	private void CreateEntityMessageReceived(NetworkConnection conn, CreateEntityMessage msg)
+	{
+		if (conn.connectionId == 0 && NetworkServer.active) // Check if we are the host
+			return;
+
+		CreateEntity(msg.id);
+	}
+
+	public override void OnEntityCreated(Entity entity)
+	{
+		if (!NetworkServer.active) return;
+		
+		base.OnEntityCreated(entity);
+
+		CreateEntityMessage msg = new CreateEntityMessage {id = entity.id};
+		NetworkServer.SendToAll(msg);
+	}
+
+	public override void OnComponentAddedToEntity(Entity entity, IComponent component)
+	{
+		if (!NetworkServer.active) return;
+
+		base.OnComponentAddedToEntity(entity, component);
 		Debug.Log("A component was added");
 	}
 
-	public override void ComponentRemovedFromEntity(IEntity entity, IComponent component)
+	public override void OnComponentRemovedFromEntity(Entity entity, IComponent component)
 	{
-		base.ComponentRemovedFromEntity(entity, component);
+		if (!NetworkServer.active) return;
+
+		base.OnComponentRemovedFromEntity(entity, component);
 		Debug.Log("A component was removed");
 	}
 
-	public override void ComponentSetOnEntity(IEntity entity, IComponent component)
+	public override void OnComponentSetOnEntity(Entity entity, IComponent component)
 	{
-		base.ComponentSetOnEntity(entity, component);
+		if (!NetworkServer.active) return;
+
+		base.OnComponentSetOnEntity(entity, component);
 		Debug.Log("A component was set");
 	}
+
 }
